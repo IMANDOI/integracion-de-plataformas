42f6df8333c85174311da8c78a0c00306b2e3524
BaironJCN <bai.campos@duocuc.cl>
2024-05-10 00:05:46 -0400

    10-05-24
    arreglitos

--- ferreteria/models.py
+++ ferreteria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    codigo = models.CharField(max_length=3, primary_key=true)
+    nombre = models.CharField(max_length=50)
+    correo = models.CharField(max_length=80)
